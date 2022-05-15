e5a4ea94230dfcab07be1f962ccbeea0b83cecca
2016112546 <awhtjddn1@naver.com>
2022-05-15 16:11:16 +0900

    알고리즘 공부

--- 백준/DFS, BFS/14502.py
+++ 백준/DFS, BFS/14502.py
@@ -1,22 +1,64 @@
 from collections import deque 
+from itertools import combinations
 
-def bfs(graph, start, visited):
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def bfs(x,y):
     # 큐 구현을 위해 deque 사용
-    queue = deque([start])
+    que = deque()
+    que.append([x,y])
     # 현재 노드를 방문 처리
-    visited[start]= True
+    visited[x][y]= True
     # 큐가 빌 때 까지 반복 
-    while queue :
+    while que :
         # 큐에서 하나의 원소를 뽑아 출력하기
-        print(queue)
-        v = queue.popleft()
-        print(v)
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i]=True
+        x,y = que.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx< N and 0<=ny <M and visited[nx][ny] and graph2[nx][ny]==0 :
+                visited[nx][ny] = False
+                graph2[nx][ny] = 2
+                que.append([nx,ny])
 
+
+def countvw():
+    for i in range(N) :
+        for j in range(M) :
+            if graph[i][j] == 2 :
+                virus.append([i,j])
+            elif graph[i][j] == 0 :
+                wall.append([i,j])
+    return
 
 N, M = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
+virus = []
+wall = []
+result = []
+countvw()
 
+wall = list(combinations(wall,3))
+
+for i in wall :
+    graph2 = [[r for r in q ] for q in graph]
+    visited  = [[True]*M for _ in range(N)]
+    for j in i :
+        x,y= j 
+        graph2[x][y] = 1 
+    
+    for k in virus :
+        x,y = k 
+        bfs(x,y)    
+
+    cnt = 0 
+    for h in graph2 :
+        for y in h :
+            if y == 0 :
+                cnt+=1 
+
+    result.append(cnt)
+
+print(max(result))
+    

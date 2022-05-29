2ae6f6da03667ea8b47327941d7a6d00741c77bd
2016112546 <awhtjddn1@naver.com>
2022-05-29 16:27:13 +0900

    싸피

---
+++ SW_Expert_Academy/2072.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for j in range(N):
+    sum = 0 
+    nums=map(int, input().split())
+    for i in nums :
+        if i%2 != 0 :
+            sum +=i 
+    
+    print("#"+str(j),sum)

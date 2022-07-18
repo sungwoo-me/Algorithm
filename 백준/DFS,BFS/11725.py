5bdaf76ccf077afc7f1fab720836481da376874f
2016112546 <awhtjddn1@naver.com>
2022-07-18 16:12:17 +0900

    백준 문제풀이

---
+++ 백준/DFS,BFS/11725.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+def dfs(start):
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i] :
+            mom[i]=start
+            dfs(i)
+
+    return
+
+
+N = int(input())
+
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+mom = [0] * (N+1)
+
+for i in range(N-1) :
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(1)
+
+for i in range(2,N+1):
+    print(mom[i])
---
+++ 백준/DFS,BFS/2644.py
@@ -0,0 +1,35 @@
+from collections import deque 
+
+def bfs(start): 
+    que = deque()
+    que.append([start,0])
+    visited[start] = True
+    while que :
+        now,count = que.popleft()
+        if now == finish :
+            return count 
+
+        count +=1
+        for i in graph[now]:
+            if not visited[i] :
+                que.append([i,count])
+        
+    return -1
+
+
+
+N=int(input())
+start, finish = map(int,input().split())
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+visited  = [False] * (N+1)
+
+
+
+for i in range(M):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(bfs(start))
